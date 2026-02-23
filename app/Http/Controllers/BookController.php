d2186ec73b85134433cc4d71a41fd4ca70f98070
helyipapa <milanmorzsa18@gmail.com>
2026-02-23 09:29:48 +0100

    first commit

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /*GET /api/books – Listázza az összes könyvet.*/
+    public function index()
+    {
+        return response()->json(Book::all(), 200);
+    }
+
+    /*GET /api/books/{query} – Listázza a query-nek megfelelő íróval vagy címmel*/
+    public function search($query)
+    {
+        $books = Book::where('title', 'like', "%$query%")->orWhere('author', 'like', "%$query%")->get();
+        return response()->json(Book::all(), 200);
+    }
+}
